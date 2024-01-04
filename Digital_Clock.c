8e91557cc23a76495026693e88e99efa163c891a
SiegBoss <146117384+SiegBoss@users.noreply.github.com>
2024-01-04 12:16:15 -0600

    Update

--- Digital_Clock.c
+++ Digital_Clock.c
@@ -1,4 +1,4 @@
-// Reloj Digital | Digital Clock
+// Reloj Digital | Digital clock
 
 // Librerias | Libraries
 #include <16F877a.h>
@@ -9,35 +9,37 @@
 #use fast_io(D)
 
 // Declaracion de variables | Variables declaration
-int y = 0, x = 0, hour = 0, minutes = 0, seconds = 0;
+int i = 0, i2 = 0, i3 = 0 , setting = 24 ,hour = 1, minutes = 0, seconds = 0;
 
 // Funcion de interrupcion Timer 1 | Timer 1 interrupt function
 #int_TIMER1
-void Reloj(void)
+void clock()
 {
     set_timer1(3200);
 
-    x++;
+    i2++;
 
-    if (x == 2)
+    if (i2 == 2)
     {
         output_toggle(PIN_B2);
-        x = 0;
+        i2 = 0;
         seconds++;
     }
 }
 
 // Funcion de interrupcion externa | External interrupt function
 #int_ext
-void Ajustar_Reloj(void)
+void settingClock()
 {
+    // Deshabilitar interrupciones del timer 1 | Disable timer 1 interrupts
     disable_interrupts(INT_TIMER1);
 
-    y++;
+    i++;
 
-    if (y > 3)
+    if (i > 4)
     {
-        y = 0;
+        i = 0;
+        // Habilitar interrupciones del timer 1 | Enable timer 1 interrupts
         enable_interrupts(INT_TIMER1);
     }
 }
@@ -48,11 +50,12 @@ void main()
     // Configuracion de los pines de entrada y salida | Input and output pins configuration
     set_tris_B(0b00000011);
     set_tris_D(0b00001000);
-    // inicializacion del LCD | LCD initialization
+    // Inicializacion del LCD | LCD initialization
     lcd_init();
-    // Configuracion de las interrupciones | Interrupts configuration
+    // Configuration del timer 1 | Timer 1 configuration
     setup_timer_1(T1_INTERNAL | T1_DIV_BY_8);
     set_timer1(3200);
+    // Habilitar interrupciones | Enable interrupts
     enable_interrupts(INT_TIMER1);
     enable_interrupts(int_ext);
     ext_int_edge(L_TO_H);
@@ -68,25 +71,25 @@ void main()
             seconds = 0;
         }
 
-        // Minutes | Minutos
+        // Minutos | Minutes
         if (minutes > 59)
         {
             hour++;
             minutes = 0;
         }
 
-        // Hour | Hora
-        if (hour > 23)
+        // Hora | Hour
+        if (hour > setting)
         {
-            hour = 0;
+            hour = 1;
         }
 
-        // Ajustar reloj | Adjust clock
-        if (y == 1)
+        // Ajustar hora | Adjust hour 
+        if (i == 1)
         {
             disable_interrupts(INT_TIMER1);
             lcd_gotoxy(1, 1);
-            printf(lcd_putc, " .:Ajustar Hor:. ");
+            printf(lcd_putc, "Ajustar Hora    ");
 
             if (input(PIN_B1) == 1)
             {
@@ -94,9 +97,9 @@ void main()
 
                 hour++;
 
-                if (hour > 23)
+                if (hour > setting)
                 {
-                    hour = 0;
+                    hour = 1;
                 }
             }
             lcd_gotoxy(1, 2);
@@ -104,11 +107,11 @@ void main()
         }
 
         // Ajustar minutos | Adjust minutes
-        else if (y == 2)
+        else if (i == 2)
         {
             disable_interrupts(INT_TIMER1);
             lcd_gotoxy(1, 1);
-            printf(lcd_putc, " .:Ajustar Min:. ");
+            printf(lcd_putc, "Ajustar Minutos ");
 
             if (input(PIN_B1) == 1)
             {
@@ -126,11 +129,11 @@ void main()
         }
 
         // Ajustar segundos | Adjust seconds
-        else if (y == 3)
+        else if (i == 3)
         {
             disable_interrupts(INT_TIMER1);
             lcd_gotoxy(1, 1);
-            printf(lcd_putc, " .:Ajustar Seg:. ");
+            printf(lcd_putc, "Ajustar Segeundos ");
 
             if (input(PIN_B1) == 1)
             {
@@ -147,13 +150,41 @@ void main()
             printf(lcd_putc, "    %2d:%2d:%2d", hour, minutes, seconds);
         }
 
+        // Configuracion del reloj de 12 o 24 horas | 12 or 24 hour clock configuration
+        else if (i == 4)
+        {
+            disable_interrupts(INT_TIMER1);
+            lcd_gotoxy(1, 1);
+            printf(lcd_putc, "Ajustar Formato  ");
+
+            if (input(PIN_B1) == 1)
+            {
+                delay_ms(250);
+                i3++;
+
+                if (i3 == 1)
+                {
+                    setting = 24;
+                }
+                else if (i3 == 2)
+                {
+                    setting = 12;
+
+                    i3 = 0;
+                }
+            }
+
+            lcd_gotoxy(1, 2);
+            printf(lcd_putc, "Formato:%2d horas", setting);
+        }
+
         // Reloj digital | Digital clock
         else
         {
             lcd_gotoxy(1, 1);
-            printf(lcd_putc, " :Reloj Digital: ");
+            printf(lcd_putc, "  Reloj Digital  ");
             lcd_gotoxy(1, 2);
-            printf(lcd_putc, "    %2d:%2d:%2d", hour, minutes, seconds);
+            printf(lcd_putc, "    %2d:%2d:%2d    ", hour, minutes, seconds);
         }
     }
 }
